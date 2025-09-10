19ec7119cd9e9f0a0a1ec4a4bf11660b475622b9
Towa Yokoyama <soccertowa0923@outlook.com>
2025-09-10 13:18:11 +0900

    cors setting

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,7 +10,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000",
+                    "https://md-s-assignment-oi3i.vercel.app",
+                    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
