fa124fe05618f297258db4d328182ad567246e90
Towa Yokoyama <soccertowa0923@outlook.com>
2025-09-10 13:08:28 +0900

    final

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
   const [timeFilter, setTimeFilter] = useState("all");
 
  
-  const API_BASE_URL = "http://localhost:8002";
+  const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
   const fetchTasks= async() => {
     setLoading(true);
