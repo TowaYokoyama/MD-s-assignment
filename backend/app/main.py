e5e26da72bfa51fdc859c5d811bd76499c531746
Towa Yokoyama <soccertowa0923@outlook.com>
2025-09-10 14:32:35 +0900

    cors eroro

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,10 +11,9 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000",
-                    "https://md-s-assignment-oi3i.vercel.app",
-                    "https://md-s-assignment-oi3i.vercel.app/",
-                    "https://md-s-assignment-7gz8.vercel.app",
-                    "https://md-s-assignment-7gz8.vercel.app/",
+                   "https://md-s-assignment-oi3i.vercel.app",
+        "https://md-s-assignment-oi3i-git-main-towa-yokoyamas-projects.vercel.app",
+        "https://md-s-assignment-oi3i-ftzc99xee-towa-yokoyamas-projects.vercel.app",
                     ],
     allow_credentials=True,
     allow_methods=["*"],
